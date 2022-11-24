c4c8622365d0105669f10e4dba2a61d9464e8aa1
Acrobaticbag3 <kevintom214@gmail.com>
2022-11-24 12:30:38 +0100

    Almost done

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Felhantering_Uppgift {
+    class Program {
+        public static void Main(string[] args) {
+            int result;
+            if (!int.TryParse(s: Console.ReadLine(), result: out result)){
+                Console.Write(value: "Input a number: ");
+            }
+        }
+
+        public static bool TryParse(string s, out int result) {
+            bool parsed;
+
+            try {
+
+                result = Convert.ToInt32(value: s);
+                parsed = true;
+
+                if (parsed == true) {
+                    Console.WriteLine(value: result);
+                    return true;
+                }
+
+            } catch (System.Exception) {
+
+                parsed = false;
+
+                if (parsed == false) {
+                    Console.WriteLine(value: "Err could not parse");
+                    
+                }
+
+            }
+
+        }
+    }
+}
